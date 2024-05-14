3c1a92773cf08c4941f21bd29cd5b5c185f810b4
baileystones <baileystones@gmail.com>
2024-05-13 20:54:55 -0600

    Trying again

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list'); 
+
+const li = document.createElement('li');
+
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = '❌';
+
+li.append(deleteButton);
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') { ... }
+  
+  deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+  });
+
+  input.value = '';
+
+  input.focus();
+  });
+
+  
