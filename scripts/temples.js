5577d93341ca7a201ae724618d12685747386bda
baileystones <baileystones@gmail.com>
2024-05-15 10:02:16 -0600

    so far, one more draft to do

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,28 @@
+const navMenu = document.querySelector(".menu-links");
+const navButton = document.querySelector("#menu");
+const title = document.querySelector("#title");
+const navLinks = document.querySelector(".menu-links");
 
+navButton.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+    navButton.classList.toggle("show");
+    navLinks.classList.toggle("show");
+})
+
+window.addEventListener("resize", function() {
+    if (this.window.innerWidth >= 740) {
+        navMenu.classList.remove("show");
+        navButton.classList.remove("show");
+        navButton.style.display = "none";
+        navLinks.style.display = "flex";
+    } else {
+        navButton.style.display = "block";
+        navLinks.style.display = "none";
+    }
+})
+
+navMenu.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A') {
+        title.textContent = event.target.textContent;
+    }
+})
